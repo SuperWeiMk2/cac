a137697a5f440fb6c079a883e18d7c50642e2b81
superweimk1 <thnecemeeony@163.com>
2024-08-18 21:55:39 +0800

    cac start

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+/* 
+    argc 是一个整数，表示传给程序的命令行参数的数量（包括程序名自身）。
+    argv 是一个字符指针数组，每个元素指向一个字符串（命令行参数）。这些字符串是以空终止的 C 风格字符串。
+ */
+int main(int avgc, char* avgv) 
+{
+    
+
+    return 0;
+}
